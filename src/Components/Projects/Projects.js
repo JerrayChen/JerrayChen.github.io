152c1af19ad9db2c342e4e2a1dee731b864867dc
JerrayChen <raychen31638@gmail.com>
2020-05-29 19:43:51 -0500

    update project css

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import './Projects.css';
 class Projects extends Component {
     constructor() {
         super();
     }
+
     render() {
+        const responsive = {
+            superLargeDesktop: {
+                breakpoint: { max: 4000, min: 3000 },
+                items: 3
+            },
+            desktop: {
+                breakpoint: { max: 3000, min: 1024 },
+                items: 2
+            },
+            tablet: {
+                breakpoint: { max: 1024, min: 464 },
+                items: 1
+            },
+            mobile: {
+                breakpoint: { max: 464, min: 0 },
+                items: 1
+            }
+        };
+
         return (
             <div className='project-wrapper'>
+
                 <p>
                     <section className='project-header'>
 
@@ -18,19 +41,20 @@ class Projects extends Component {
                             <i class="far fa-window-restore icon"></i>
                         </a>
                     </section>
-                    <section>
+                    <section className='project-info'>
                         A mobile-base app that user can buy homemade food
                     </section>
                 </p>
-
-                <img src="/ProjectsPics/Jiabon1.png" alt="image" />
-                <img src="/ProjectsPics/Jiabon2.png" alt="image" />
-                <img src="/ProjectsPics/Jiabon3.png" alt="image" />
-                <img src="/ProjectsPics/Jiabon4.png" alt="image" />
+                <Carousel responsive={responsive}>
+                    <img src="/ProjectsPics/Jiabon1.png" alt="image" />
+                    <img src="/ProjectsPics/Jiabon2.png" alt="image" />
+                    <img src="/ProjectsPics/Jiabon3.png" alt="image" />
+                    <img src="/ProjectsPics/Jiabon4.png" alt="image" />
+                </Carousel>
                 <p>
                     <section className='project-header'>
 
-                        <h2>Meal Thyme</h2>
+                        <h2 className='project-title'>Meal Thyme</h2>
                         <a href='https://github.com/dm27-meal-planner/meal-planner' target="_blank">
                             <i class="fab fa-github-square icon"></i>
                         </a>
@@ -38,30 +62,36 @@ class Projects extends Component {
                             <i class="far fa-window-restore icon"></i>
                         </a>
                     </section>
-                    <section>
-                        A meal plans website that user can manage recipes, and generate grocery lists.
+                    <section className='project-info'>
+                        A meal planning website that user can manage recipes, and generate grocery lists.
                     </section>
                 </p>
-                <img src="/ProjectsPics/MT1.png" alt="image" />
-                <img src="/ProjectsPics/MT2.png" alt="image" />
-                <img src="/ProjectsPics/MT3.png" alt="image" />
-                <img src="/ProjectsPics/MT4.png" alt="image" />
+                <Carousel responsive={responsive}>
+                    <img src="/ProjectsPics/MT1.png" alt="image" />
+                    <img src="/ProjectsPics/MT2.png" alt="image" />
+                    <img src="/ProjectsPics/MT3.png" alt="image" />
+                    <img src="/ProjectsPics/MT4.png" alt="image" />
+                </Carousel>
+
                 <p>
                     <section className='project-header'>
 
-                    <h2>Team Rocket Market</h2>
-                    <a href='https://github.com/JerrayChen/team-rocket-market' target="_blank">
-                        <i class="fab fa-github-square icon"></i>
-                    </a>
+                        <h2 className='project-title'>Team Rocket Market</h2>
+                        <a href='https://github.com/JerrayChen/team-rocket-market' target="_blank">
+                            <i class="fab fa-github-square icon"></i>
+                        </a>
                     </section>
-                    <section>
+                    <section className='project-info'>
                         A website for trading Pokemon
                     </section>
                 </p>
-                <img src="/ProjectsPics/TRM1.png" alt="image" />
-                <img src="/ProjectsPics/TRM2.png" alt="image" />
-                <img src="/ProjectsPics/TRM3.png" alt="image" />
-                <img src="/ProjectsPics/TRM4.png" alt="image" />
+                <Carousel responsive={responsive}>
+                    <img src="/ProjectsPics/TRM1.png" alt="image" />
+                    <img src="/ProjectsPics/TRM2.png" alt="image" />
+                    <img src="/ProjectsPics/TRM3.png" alt="image" />
+                    <img src="/ProjectsPics/TRM4.png" alt="image" />
+                </Carousel>
+
 
             </div>
         )
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -11,46 +11,46 @@ class Skills extends Component {
             <div className='skills'>
 
                 <div className="skill-wrapper">
-                    <section>
+                    <section className='skill-block'>
                         <i class="fab fa-js-square"></i><span>Javascript</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="icon-reactjs"></i><span>React</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="icon-nodejs"></i><span>NodeJS</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="icon-csharp"></i><span>C#.NET</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="icon-python"></i><span>Python</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="icon-cplusplus"></i><span>C++</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="icon-java"></i><span>Java</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="icon-oracle"></i><span>Oracle SQL</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="icon-postgres"></i><span>PostgreSQL</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="icon-npm"></i><span>NPM</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="icon-sass"></i><span>Sass</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="fab fa-html5"></i><span>HTML 5</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="fab fa-css3-alt"></i><span>CSS 3</span>
                     </section>
-                    <section>
+                    <section className='skill-block'>
                         <i class="icon-git"></i><span>Git</span>
                     </section>
                 </div>
